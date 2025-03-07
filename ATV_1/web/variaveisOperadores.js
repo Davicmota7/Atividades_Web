e3144c0399eab775cb7f23bf25b214b60d76a260
Davicmota7 <135145240+Davicmota7@users.noreply.github.com>
2025-03-07 09:44:14 -0300

    Add files via upload

---
+++ ATV_1/web/variaveisOperadores.js
@@ -0,0 +1,19 @@
+
+let numA = 90, numB = 2;
+let resultadoSoma = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${resultadoSoma}`);
+
+let resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultadoSubtracao}`);
+
+let resultadoMultiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${resultadoMultiplicacao}`);
+
+let resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultadoDivisao}`);
+
+let resultadoModulo = numA % numB;
+console.log(`Resultado do módulo entre ${numA} e ${numB} é ${resultadoModulo}`);
+
+let resultadoExponenciacao = numA ** numB;
+console.log(`Resultado da exponenciação entre ${numA} e ${numB} é ${resultadoExponenciacao}`);
